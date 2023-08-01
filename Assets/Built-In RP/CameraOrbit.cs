ea20dd534c3545efbb0b8c03a7717ef339f2c507
aashiq <ashaik11@asu.edu>
2023-07-31 23:39:14 -0700

    added rotation script and webrtc samples

---
+++ Assets/Built-In RP/CameraOrbit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbit : MonoBehaviour
+{
+    public Transform target; //Purpose of the object
+    public Vector2 angle; //Purpose of the object
+    public Vector3 offset; //Purpose of the object
+    public float distance = 5; //Distance
+
+
+    private void Update()
+    {
+        Quaternion rotation = Quaternion.Euler(angle.x,angle.y,0); 
+        Vector3 position = rotation * new Vector3(0, 0, -distance) + target.position + offset;
+
+        transform.position = position;
+        transform.rotation = rotation;
+
+        angle.y += Time.deltaTime * 100;
+        angle.y %= 360;
+    }
+
+}
